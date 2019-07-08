b6532a8b559001e5c4caaf58e005b82ca162a834
Matthew Listor <mattlistor@gmail.com>
2019-07-08 18:21:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 1
 second_number = 2 
-
-def calculator(first_number, second_number) 
-  sum = first_number + second_number
-end
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
